d72f3e4e785f476f9d6dcd5a7ef400ac97cdca72
Niklas Amslgruber <niklas.amslgruber@me.com>
2020-10-23 13:03:43 +0200

    Plot all results

--- analyze_results.py
+++ analyze_results.py
@@ -1,7 +1,11 @@
+import os
+
 import matplotlib.pyplot as plt
 from config import FLAGS
 from helper import file_utils as file, io_utils as io
 import pandas as pd
+
+available_datasets = ["r8", "mr", "ohsumed", "r52", "20ng"]
 
 
 def visualize_loss(loss_array, loss):
@@ -16,8 +20,10 @@ def visualize_loss(loss_array, loss):
     plt.close(fig)
 
 
-def analyze_results():
-    data = file.get_eval_logs()
+def analyze_results(dataset):
+    if not os.path.isfile(io.get_eval_log_path(dataset)):
+        return None
+    data = file.get_eval_logs(dataset)
     window_size = set(data["window_size"].tolist())
     raw_count = set(data["raw_count"].tolist())
     threshold = set(data["threshold"].tolist())
@@ -46,16 +52,16 @@ def analyze_results():
     results_df.columns = ["count"] + data.columns[1:].tolist()
     results_df = results_df.sort_values(by=["window_size", "threshold"]).reset_index(drop=True)
     results_df = results_df.dropna()
-    print(f"Average results for {FLAGS.dataset}")
-    print(results_df)
-    plot_results(results_df)
+    return results_df
 
 
-def plot_results(data, metric="accuracy"):
+def plot_results(data, ax, metric, dataset):
     raw_count = set(data["raw_count"].tolist())
-    raw_count.remove("NaN")
+    if "NaN" in raw_count:
+        raw_count.remove("NaN")
     thresholds = list(set(data["threshold"].tolist()))
-    thresholds.remove("NaN")
+    if "NaN" in thresholds:
+        thresholds.remove("NaN")
 
     wiki_cond = data["wiki_enabled"] == True
     window_cond = data["window_size"] == 15
@@ -69,15 +75,14 @@ def plot_results(data, metric="accuracy"):
         metric_dict = metric_data.to_dict()[metric]
         data_dict[r] = metric_dict
 
-    base = data[data["wiki_enabled"] == False][metric].tolist()
-    assert len(base) == 1
+    base = data[(data["wiki_enabled"] == False) & window_cond][metric].tolist()
+    assert len(base) <= 1
     data_dict["no_wiki"] = base
 
-    fig, ax = plt.subplots(figsize=(15, 15))
-    fig.suptitle(f"Results by threshold (metric: {metric})")
-    plt.xlabel("threshold")
-    plt.ylabel(metric)
-    plt.xticks(thresholds)
+    ax.set_xlabel("threshold")
+    ax.set_ylabel(metric)
+    ax.title.set_text(dataset)
+    ax.set_xticks(thresholds)
 
     assert len(data_dict.keys()) == 3
     for key in data_dict:
@@ -85,24 +90,39 @@ def plot_results(data, metric="accuracy"):
         name = "Raw" if key else "IDF"
         color = "b" if key else "g"
         if key == "no_wiki":
-            tmp = [value[int(t)] for t in value]
-            assert len(tmp) == 1
-            metrics = tmp * len(thresholds)
-            name = "textGCN"
-            color = "r"
+            if len(value) > 0:
+                tmp = [value[int(t)] for t in value]
+                assert len(tmp) == 1
+                metrics = tmp * len(thresholds)
+                name = "textGCN"
+                color = "r"
         else:
             metrics = [value[t] for t in value]
         assert len(metrics) == len(thresholds)
-        print(metrics, thresholds)
-        plt.plot(thresholds, metrics, color, label=name, linewidth=4)
+        ax.plot(thresholds, metrics, color, label=name, linewidth=2)
+
+    ax.legend()
+
+
+def plot_all(metric="accuracy"):
+    number_subplots = len(available_datasets)
+    fig, ax = plt.subplots(number_subplots, figsize=(15, 15))
+    fig.suptitle(f"Results by threshold (metric: accuracy)")
+
+    data_dict = {}
+    for index, dataset in enumerate(available_datasets):
+        results_df = analyze_results(dataset)
+        data_dict[dataset] = results_df
+        if results_df is not None:
+            plot_results(results_df, ax[index], metric, dataset)
+    assert len(data_dict.keys()) == number_subplots
 
     plt.tight_layout()
-    plt.legend()
-    plt.savefig(f"{FLAGS.dataset}_{metric}.png")
+    plt.savefig(f"{io.get_basic_plots_path()}/dataset_results.png")
+    plt.close(fig)
 
 
-def plot_edges():
-    available_datasets = ["r8", "mr", "ohsumed", "r52", "20ng"]
+def plot_number_of_edges():
     thresholds = range(1, 85)
     dataset_length = {}
     for dataset in available_datasets:
@@ -136,5 +156,5 @@ def plot_edges():
 
 
 if __name__ == '__main__':
-    analyze_results()
-    # plot_edges()
+    # analyze_results()
+    plot_all()
