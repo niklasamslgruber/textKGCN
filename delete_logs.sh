def48356c12b38aebc0aafe454058b8c92008cd6
Niklas Amslgruber <niklas.amslgruber@me.com>
2020-12-28 12:37:57 +0100

    Disable logging

---
+++ delete_logs.sh
@@ -0,0 +1,5 @@
+rm -r -f ~/Desktop/textKGCN/_data/mr/_logs &&
+rm -r -f ~/Desktop/textKGCN/_data/r8/_logs &&
+rm -r -f ~/Desktop/textKGCN/_data/r52/_logs &&
+rm -r -f ~/Desktop/textKGCN/_data/ohsumed/_logs &&
+rm -r -f ~/Desktop/textKGCN/_data/20ng/_logs
